f3c28d04b7e27f2ffc46d6c9fa13336979cc2690
asaph <asaph@gmail.com>
2022-04-03 17:20:48 -0300

    adicionar rotinha /api/tempo

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,24 @@
-function tempo (request,response) {
+function tempo(request, response) {
     const dynamicDate = new Date();
 
+    carregarDados = function () {
+
+        token = localStorage.getItem("userToken");
+
+        $http({
+            method: 'GET',
+            url: 'http://localhost:3000/Avaliacao1'
+        }).then(function (response) {
+            $scope.distritos = response.data;
+
+        }, function (response) {
+            console.log(response.data);
+            console.log(response.status);
+        });
+    };
+
     response.json({
-        date : dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString()
     });
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
 function Home() {
     return <div>Home</div>
+
+    carregarDados = function() {
+
+        token = localStorage.getItem("userToken");
+
+        $http({
+            method : 'GET',
+            url : 'http://localhost:8080/user/distritos'
+        }).then(function(response) {
+            $scope.distritos = response.data;
+
+        }, function(response) {
+            console.log(response.data);
+            console.log(response.status);
+        });
+    }; 
 }
 
 export default Home
